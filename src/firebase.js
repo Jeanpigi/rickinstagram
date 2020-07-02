7c992ad641f212cc4df6eab39d4c2a150d63df1a
Jean Pierre <stick-master666@hotmail.com>
2020-07-02 15:43:59 -0500

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firebase-firestore';
+import firebase from "firebase/app";
+import "firebase/firebase-firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyA-WXJszG8KW1NO8IALvEc8OhPwaf_Vd9g",
-    authDomain: "rickinstagram-9cd26.firebaseapp.com",
-    databaseURL: "https://rickinstagram-9cd26.firebaseio.com",
-    projectId: "rickinstagram-9cd26",
-    storageBucket: "rickinstagram-9cd26.appspot.com",
-    messagingSenderId: "406284896955",
-    appId: "1:406284896955:web:fd0c2fd9f3c6b3b9f2f79e"
+  apiKey: "APIKEY",
+  authDomain: "AUTHDOMAIN",
+  databaseURL: "DATABASE_URL",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGEBUCKET",
+  messagingSenderId: "MESSAGINSENDER_ID",
+  appId: "APPID",
 };
-  // Initialize Firebase
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export const database = firebase.firestore();
